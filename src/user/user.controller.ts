01a525221cfb7b689bcbf39fc3d9bc590d638a25
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-06-20 23:35:12 -0300

    feat: add more endpoints and update doc

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,9 @@ import {
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { IsPublic } from '../auth/decorator/is-public.decorator'
 
+@IsPublic()
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
@@ -21,7 +23,22 @@ export class UserController {
     }
 
     @Get()
-    getAllUsers() {
+    findAll() {
         return this.userService.findAll()
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string) {
+        return this.userService.findById(id)
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.update(id, updateUserDto)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.userService.delete(id)
     }
 }
